4b5c91e4affdd675e350e1ee49614e0d218711ec
Albert <albert.fz@yandex.ru>
2022-12-26 08:59:37 +0500

    fix strange custom allocators code

--- gds/alloc.h
+++ gds/alloc.h
@@ -1,8 +1,8 @@
 #ifdef ALLOCATOR
-    #define ALLOC(T, size)           $(T *) (ALLOCATOR, alloc)(size * sizeof(T))
-    #define CALLOC(T, size)          $(T *) (ALLOCATOR, calloc)(size, sizeof(T))
-    #define REALLOC(T, memptr, size) $(T *) (ALLOCATOR, realloc)(memptr, size * sizeof(T))
-    #define DEALLOC(memptr)          $(ALLOCATOR, dealloc)(memptr)
+    #define ALLOC(T, size)           (T *) $T(ALLOCATOR, alloc)(size * sizeof(T))
+    #define CALLOC(T, size)          (T *) $T(ALLOCATOR, calloc)(size, sizeof(T))
+    #define REALLOC(T, memptr, size) (T *) $T(ALLOCATOR, realloc)(memptr, size * sizeof(T))
+    #define DEALLOC(memptr)          $T(ALLOCATOR, dealloc)(memptr)
 #else
     #define ALLOC(T, size)           (T *) malloc(size * sizeof(T))
     #define CALLOC(T, size)          (T *) calloc(size, sizeof(T))
